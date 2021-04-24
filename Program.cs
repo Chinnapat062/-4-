d587155e8443e0538d198c4d7420af84acd5e98e
Chinnapat062 <78645438+Chinnapat062@users.noreply.github.com>
2021-04-24 15:44:58 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ทำข้อสอบ4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x,x1,x2, y,y1,y2, dx, dy, step;
+            x1 = float.Parse(Console.ReadLine());
+            y1 = float.Parse(Console.ReadLine());
+            x2 = float.Parse(Console.ReadLine());
+            y2 = float.Parse(Console.ReadLine());
+
+            dx = x2 - x1;
+            dy = y2 - y1;
+
+            if(dx>=dy)
+            {
+                step = dx;
+            }
+            else if (dx<=dy)
+            {
+                step=dy;
+            }
+            dx /= step;
+            dy /= step;
+            x = x1;
+            y = y1;
+
+            int i = 1;
+            do
+            {
+                    Console.WriteLine(x, ",", y);
+                x += dx;
+                y += dy;
+                i++;
+            } while (i <= step);
+
+        }
+    }
+}
